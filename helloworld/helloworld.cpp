56183f1e7d45ac47b3701bb8d4740b12e85a6b4a
Michał Przybylak <michal.przybylak@gmail.com>
2014-12-09 07:15:30 +0000

    hello world example

---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "Podaj imie leszczu" << endl;
+	string first_name;
+	cin >> first_name;
+
+
+	cout << "Hello, " << first_name << endl;
+	return 0;
+}
