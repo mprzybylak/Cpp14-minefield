ec20dd6957ea3d14877a91eba4bdaa9ae547867f
Michal Przybylak <michal.przybylak@gmail.com>
2014-12-10 11:05:46 +0100

    uzupelnienie przykladow helloworld

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -4,11 +4,32 @@ using namespace std;
 
 int main() {
 
+	constexpr double pi = 3.14;
+	const double a = 2;
+
 	cout << "Podaj imie leszczu" << endl;
+
 	string first_name;
 	cin >> first_name;
 
+	cout << "Podaj wiek" << endl;
 
-	cout << "Hello, " << first_name << endl;
+	int age;
+	cin >> age;
+
+	switch(age) {
+		case 18:
+			cout << "Najs..." << endl;
+			break;
+		default:
+			cout << "Slabo..." << endl;
+			break;
+	}
+
+	double(pi);
+	double{pi};
+
+	cout << "Witaj, " << first_name << endl;
+
 	return 0;
-}
+}  
