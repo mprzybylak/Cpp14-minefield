ea456fa9a51c9b3464f11eaafdf44474ff065219
Michał Przybylak <michal.przybylak@gmail.com>
2014-12-18 12:47:46 +0000

    'project'-like structure introduced

---
+++ src/io/simple_make.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#!/bin/bash
+
+clear
+
+rm -r target/*
+cd ./src
+g++ -std=c++0x -Wall -Werror -pedantic $1.cpp -o $1.out
+cd ..
+mv ./src/$1.out ./target
+cp ./resources/* target
+
+clear
+cd ./target
+./$1.out
+
+cd ..
